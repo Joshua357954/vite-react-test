ce84ac3c67803adcce851234181cac928d9ae312
Joshuaboyi <boyijoshua72@gmail.com>
2022-05-10 05:16:46 +0100

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,24 @@ function App() {
 
   return (
     <div className="App">
+
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite / React!</p>
+
+        <p>Hello Josh / Jay </p>
+
         <p>
-          <button type="button" onClick={() => setCount((count) => Count + 1)}>
-            Count => {count}
+
+          <button type="button" onClick={() => setCount((count) => count + Math.floor(Math.random()*5))}>
+            New Num == {count}
+
           </button>
+
         </p>
-        <p>
-          Edit <code> App.js or jsx </code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Documentatio
-          </a>
-        </p>
+
       </header>
+
     </div>
   )
 }
